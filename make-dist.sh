cf0531ead0c66dbaf2414e580bb29ef47993dbf3
Daniel Pocock <daniel@pocock.com.au>
2013-02-23 18:11:53 +0100

    Add script for making source tarball for release/distribution

---
+++ make-dist.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# releases a source tarball for release to users who need
+# to build from source
+
+# many packaging systems refuse to accept minified Javascript
+# code as a release
+
+set -e
+
+PROJECT=sipml5
+
+if [ -z "$VERSION" ];
+then
+  VERSION=`date +%Y%m%d.%H%M`
+fi
+
+RELEASE_NAME=${PROJECT}-${VERSION}
+
+DIST_TGZ=${RELEASE_NAME}.tar.gz
+DEB_DIST_TGZ=${PROJECT}_${VERSION}.orig.tar.gz
+
+mkdir ${RELEASE_NAME}
+
+cp -dpR \
+  assets \
+  *.htm *.html \
+  COPYING \
+  *.sh \
+  docs \
+  images \
+  SIPml-api.js SIPml.js \
+  sounds \
+  src \
+  tests \
+  ${RELEASE_NAME}/
+
+tar czf ${DIST_TGZ} ${RELEASE_NAME}
+rm -rf ${RELEASE_NAME}
+
+cp ${DIST_TGZ} ../${DEB_DIST_TGZ}
+
